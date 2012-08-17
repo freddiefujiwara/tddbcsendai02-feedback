7c2478390eb09d0d4ac7aa27bb157ae4ce898401
kashimoo <kashimama3hiro@gmail.com>
2012-08-17 18:04:43 +0900

    finish step0

--- VendingMachine.js
+++ VendingMachine.js
@@ -1,4 +1,12 @@
 var VendingMachine  = function(){
 	this.total = 0;
 };
+VendingMachine.prototype.insert = function(coin){
+	this.total += coin;
+};
+VendingMachine.prototype.refund = function(){
+	var change = this.total;
+	this.total = 0;
+	return change;
+};
 module.exports = VendingMachine;
--- test/step0.js
+++ test/step0.js
@@ -1,9 +1,30 @@
-var assert = require('assert');
+var should = require('should');
 var VendingMachine = require('../VendingMachine');
-var vm = new VendingMachine();
 
-describe('insert and refund', function() {
-	it('total is 0', function() {
-		assert.equal(vm.total, 0);
+describe('Vending Machine Step0', function() {
+
+	var vm;
+	beforeEach(function(){
+		vm = new VendingMachine();
+		vm.insert(10);
+		vm.insert(50);
+		vm.insert(100);
+		vm.insert(500);
+		vm.insert(1000);
+	});
+
+	describe('insert 10, 50, 100, 500 and 1000', function() {
+		it('total is 1660', function() {
+			vm.total.should.equal(1660);
+		});
+	});
+
+	describe('insert 10, 50, 100, 500 and 1000 then refund', function() {
+		it('total is 0', function() {
+			var change = vm.refund();
+			vm.total.should.equal(0);
+			change.should.equal(1660);
+		});
 	});
 });
+
